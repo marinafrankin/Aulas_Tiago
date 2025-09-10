f9d92334d21cca7275fa5b989947a2a3f00fffe4
Marina Frankin <marinafrankin2005@gmail.com>
2025-09-10 09:43:11 -0300

    10/09/2025 - 09:43

---
+++ agendamento.js
@@ -0,0 +1,303 @@
+// AGENDAMENTOS[
+  [
+  {
+    "id": "agendamento_1",
+    "id_animal": "animal_1",
+    "data_agendamento": "2025-09-03T14:00:00Z",
+    "serviço": "Banho e Tosa"
+  },
+  {
+    "id": "agendamento_2",
+    "id_animal": "animal_2",
+    "data_agendamento": "2025-09-03T15:30:00Z",
+    "serviço": "Vacinação"
+  },
+  {
+    "id": "agendamento_3",
+    "id_animal": "animal_3",
+    "data_agendamento": "2025-09-04T10:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_4",
+    "id_animal": "animal_4",
+    "data_agendamento": "2025-09-04T11:30:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_5",
+    "id_animal": "animal_5",
+    "data_agendamento": "2025-09-04T14:00:00Z",
+    "serviço": "Tosa Higiênica"
+  },
+  {
+    "id": "agendamento_6",
+    "id_animal": "animal_6",
+    "data_agendamento": "2025-09-05T09:00:00Z",
+    "serviço": "Tosa"
+  },
+  {
+    "id": "agendamento_7",
+    "id_animal": "animal_7",
+    "data_agendamento": "2025-09-05T10:30:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_8",
+    "id_animal": "animal_8",
+    "data_agendamento": "2025-09-05T15:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_9",
+    "id_animal": "animal_9",
+    "data_agendamento": "2025-09-06T11:00:00Z",
+    "serviço": "Banho e Tosa"
+  },
+  {
+    "id": "agendamento_10",
+    "id_animal": "animal_10",
+    "data_agendamento": "2025-09-06T13:00:00Z",
+    "serviço": "Vacinação"
+  },
+  {
+    "id": "agendamento_11",
+    "id_animal": "animal_11",
+    "data_agendamento": "2025-09-07T10:30:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_12",
+    "id_animal": "animal_12",
+    "data_agendamento": "2025-09-07T14:00:00Z",
+    "serviço": "Tosa"
+  },
+  {
+    "id": "agendamento_13",
+    "id_animal": "animal_13",
+    "data_agendamento": "2025-09-08T09:30:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_14",
+    "id_animal": "animal_14",
+    "data_agendamento": "2025-09-08T11:00:00Z",
+    "serviço": "Banho e Tosa"
+  },
+  {
+    "id": "agendamento_15",
+    "id_animal": "animal_15",
+    "data_agendamento": "2025-09-09T10:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_16",
+    "id_animal": "animal_16",
+    "data_agendamento": "2025-09-09T14:30:00Z",
+    "serviço": "Vacinação"
+  },
+  {
+    "id": "agendamento_17",
+    "id_animal": "animal_17",
+    "data_agendamento": "2025-09-10T09:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_18",
+    "id_animal": "animal_18",
+    "data_agendamento": "2025-09-10T11:00:00Z",
+    "serviço": "Banho e Tosa"
+  },
+  {
+    "id": "agendamento_19",
+    "id_animal": "animal_19",
+    "data_agendamento": "2025-09-11T10:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_20",
+    "id_animal": "animal_20",
+    "data_agendamento": "2025-09-11T14:00:00Z",
+    "serviço": "Tosa Higiênica"
+  },
+  {
+    "id": "agendamento_21",
+    "id_animal": "animal_21",
+    "data_agendamento": "2025-09-12T09:30:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_22",
+    "id_animal": "animal_22",
+    "data_agendamento": "2025-09-12T11:00:00Z",
+    "serviço": "Tosa"
+  },
+  {
+    "id": "agendamento_23",
+    "id_animal": "animal_23",
+    "data_agendamento": "2025-09-13T10:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_24",
+    "id_animal": "animal_24",
+    "data_agendamento": "2025-09-13T14:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_25",
+    "id_animal": "animal_25",
+    "data_agendamento": "2025-09-14T10:30:00Z",
+    "serviço": "Banho e Tosa"
+  },
+  {
+    "id": "agendamento_26",
+    "id_animal": "animal_26",
+    "data_agendamento": "2025-09-14T15:00:00Z",
+    "serviço": "Vacinação"
+  },
+  {
+    "id": "agendamento_27",
+    "id_animal": "animal_27",
+    "data_agendamento": "2025-09-15T09:00:00Z",
+    "serviço": "Tosa Higiênica"
+  },
+  {
+    "id": "agendamento_28",
+    "id_animal": "animal_28",
+    "data_agendamento": "2025-09-15T11:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_29",
+    "id_animal": "animal_29",
+    "data_agendamento": "2025-09-16T10:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_30",
+    "id_animal": "animal_30",
+    "data_agendamento": "2025-09-16T14:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_31",
+    "id_animal": "animal_31",
+    "data_agendamento": "2025-09-17T09:30:00Z",
+    "serviço": "Banho e Tosa"
+  },
+  {
+    "id": "agendamento_32",
+    "id_animal": "animal_32",
+    "data_agendamento": "2025-09-17T11:00:00Z",
+    "serviço": "Vacinação"
+  },
+  {
+    "id": "agendamento_33",
+    "id_animal": "animal_33",
+    "data_agendamento": "2025-09-18T10:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_34",
+    "id_animal": "animal_34",
+    "data_agendamento": "2025-09-18T14:00:00Z",
+    "serviço": "Tosa Higiênica"
+  },
+  {
+    "id": "agendamento_35",
+    "id_animal": "animal_35",
+    "data_agendamento": "2025-09-19T09:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_36",
+    "id_animal": "animal_36",
+    "data_agendamento": "2025-09-19T10:30:00Z",
+    "serviço": "Tosa"
+  },
+  {
+    "id": "agendamento_37",
+    "id_animal": "animal_37",
+    "data_agendamento": "2025-09-20T11:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_38",
+    "id_animal": "animal_38",
+    "data_agendamento": "2025-09-20T15:00:00Z",
+    "serviço": "Banho e Tosa"
+  },
+  {
+    "id": "agendamento_39",
+    "id_animal": "animal_39",
+    "data_agendamento": "2025-09-21T10:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_40",
+    "id_animal": "animal_40",
+    "data_agendamento": "2025-09-21T14:30:00Z",
+    "serviço": "Vacinação"
+  },
+  {
+    "id": "agendamento_41",
+    "id_animal": "animal_41",
+    "data_agendamento": "2025-09-22T09:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_42",
+    "id_animal": "animal_42",
+    "data_agendamento": "2025-09-22T11:00:00Z",
+    "serviço": "Limpeza de Aquário"
+  },
+  {
+    "id": "agendamento_43",
+    "id_animal": "animal_43",
+    "data_agendamento": "2025-09-23T10:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_44",
+    "id_animal": "animal_44",
+    "data_agendamento": "2025-09-23T14:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_45",
+    "id_animal": "animal_45",
+    "data_agendamento": "2025-09-24T09:30:00Z",
+    "serviço": "Banho e Tosa"
+  },
+  {
+    "id": "agendamento_46",
+    "id_animal": "animal_46",
+    "data_agendamento": "2025-09-24T11:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_47",
+    "id_animal": "animal_47",
+    "data_agendamento": "2025-09-25T10:00:00Z",
+    "serviço": "Tosa Higiênica"
+  },
+  {
+    "id": "agendamento_48",
+    "id_animal": "animal_48",
+    "data_agendamento": "2025-09-25T14:00:00Z",
+    "serviço": "Banho"
+  },
+  {
+    "id": "agendamento_49",
+    "id_animal": "animal_49",
+    "data_agendamento": "2025-09-26T09:00:00Z",
+    "serviço": "Consulta Veterinária"
+  },
+  {
+    "id": "agendamento_50",
+    "id_animal": "animal_50",
+    "data_agendamento": "2025-09-26T10:30:00Z",
+    "serviço": "Banho e Tosa"
+  }
+]
